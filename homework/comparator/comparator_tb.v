f6dfbf6da4baff569f07200fd373e508ab2990f3
MistyBlunch <gracenikole@gmail.com>
2020-09-26 01:20:09 -0500

    yanooo

--- homework/comparator/comparator_tb.v
+++ homework/comparator/comparator_tb.v
@@ -6,9 +6,20 @@ module comparator_tb();
   comparator comparator(AB,CD,F1,F2,F3);
 
   initial begin
+    $display("AB = ", AB, "\nCD = ", CD);
+    $dumpfile("comparador.vcd");
+    $dumpvars();
     AB = 1;
     CD = 0;
-    #1
+    #5
+    $display("AB = ", AB, "\nCD = ", CD);
+    AB = 1;
+    CD = 1;
+    #5
+    $display("AB = ", AB, "\nCD = ", CD);
+    AB = 0;
+    CD = 1;
+    #5
     $display("AB = ", AB, "\nCD = ", CD);
   end
 endmodule
