d9bfb81d0e153afdee4fb0137596e4b5239f855b
MistyBlunch <gracenikole@gmail.com>
2020-09-25 23:55:49 -0500

    :alien: non

--- homework/demux_1_16/demux_1_16_tb.v
+++ homework/demux_1_16/demux_1_16_tb.v
@@ -2,17 +2,19 @@
 `include "demux_1_16.v"
 
 module demux_1_16_tb();
-  reg [15:0] i;
+  reg [15:0] i,z;
   reg [3:0] sel;
   wire [15:0] a,b,c,d,e,f,g,h,j,k,l,m,n,s,o,p;
 
   demux_1_16 demux_1_16(i,sel,a,b,c,d,e,f,g,h,j,k,l,m,n,s,o,p);
 
   initial begin
-    i = 1;
-    sel = 5;
-    #1
-    $display("a =", a, "\nb =", b, "\nc =", c, "\nd =", d, "\ne =", e, "\nf =", f, "\ng =", g, "\nh =", h, "\nj =", j, "\nk =", k, "\nl =", l, "\nm =", m, "\nn =", n, "\ns =" ,s, "\no =", o, "\np =", p, "\nsel =", sel, "\ni =", i);
+    for(z = 0; z < 16; ++z) begin
+      i = 3;
+      sel = z;
+      #5
+      $display("a =", a, " b =", b, " c =", c, " d =", d, " e =", e, " f =", f, " g =", g, " h =", h, " j =", j, " k =", k, " l =", l, " m =", m, " n =", n, " s =" ,s, " o =", o, " p =", p, " sel =", sel, " i =", i);
+    end
     
     $dumpfile("mux_1_16_tb.vcd");
     $dumpvars();
