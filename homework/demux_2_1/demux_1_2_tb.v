c183b3937b9764b3ce9cba7c18a47a508f745f8f
MistyBlunch <gracenikole@gmail.com>
2020-09-26 00:54:10 -0500

    :star: amen

--- homework/demux_2_1/demux_1_2_tb.v
+++ homework/demux_2_1/demux_1_2_tb.v
@@ -1,15 +1,19 @@
+`include "demux_1_2.v"
+
 module demux_1_2_tb ();
-  reg [15:0] i;
+  reg [15:0] i,z;
   reg sel;
   wire [15:0] x, y;
 
 demux_1_2 demux_1_2 (i,sel,x,y);
 
 initial begin
-  i = 1;
-  sel = 0;
-  #1
-  $display ("x = ",x, "y = ", y);
+  for(z = 0; z < 2; ++z) begin
+    i = 3;
+    sel = z;
+    #5
+    $display ("x = ",x, " y = ", y, " sel = ", sel, " i = ", i);
+  end
 
   $dumpfile("mux_2_1_tb.vcd");
   $dumpvars();
--- homework/mux_16_1/mux_16_1_tb.v
+++ homework/mux_16_1/mux_16_1_tb.v
@@ -1,20 +1,26 @@
 `include "mux_16_1.v"
 module mux_16_1_tb();
-reg [15:0] a,b,c,d,e,f,g,h,i,j,k,l,m,n,s,o;
+reg [15:0] a,b,c,d,e,f,g,h,i,j,k,l,m,n,s,o,z;
 reg [3:0] sel;
 wire [15:0] y;
 
 mux_16_1 mux_16_1(a,b,c,d,e,f,g,h,i,j,k,l,m,n,s,o,sel,y);
 
 initial begin
-    a = 1; b = 2; c = 3; d = 4;
-    e = 5; f = 6; g = 7; h = 8;
-    i = 9; j = 10; k = 11; l = 12;
-    m = 13; n = 14; s = 15; o = 16;
-    sel = 0;
-    #1
-    $display("\na =", a, "\nb =", b, "\nc =", c, "\nd =", d, "\ne =", e, "\nf =", f, "\ng =", g, "\nh =", h, "\ni =", i, "\nj =", j, "\nk =", k, "\nl =", l, "\nm =", m, "\nn =", n, "\ns =" ,s, "\no =", o, "\nsel =", sel, "\ny =", y);
-    $dumpfile("mux_16_1_tb.vcd");
-    $dumpvars();
+  $display(" a =", a, " b =", b, " c =", c, " d =", d, " e =", e, " f =", f, " g =", g, " h =", h, " i =", i, " j =", j, " k =", k, " l =", l, " m =", m, " n =", n, " s =" ,s, " o =", o, " sel =", sel, " y =", y);
+  $dumpfile("mux_16_1_tb.vcd");
+  $dumpvars();
+  
+  a = 1212; b = 23; c = 0; d = 432;
+  e = 51; f = 61; g = 17; h = 38;
+  i = 91; j = 140; k = 111; l = 124;
+  m = 113; n = 14; s = 155; o = 165;
+
+  for(z = 0; z < 16; ++z) begin
+    i = 3;
+    sel = z;
+    #3
+    $display(" a =", a, " b =", b, " c =", c, " d =", d, " e =", e, " f =", f, " g =", g, " h =", h, " i =", i, " j =", j, " k =", k, " l =", l, " m =", m, " n =", n, " s =" ,s, " o =", o, " sel =", sel, " y =", y);
+  end
 end
 endmodule
